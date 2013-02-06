e73f46d965753ea57aa7b1bdd6c4ad04fd658c38
Buwei Chiu <bu@hax4.in>
2013-02-06 20:25:21 +0800

    implmented: async waterfall example

--- async.waterfall.js
+++ async.waterfall.js
@@ -1,7 +1,15 @@
 var async = require("async");
 
 async.waterfall([
-    
+    function buySomething(callback) {
+        return callback();
+    },
+
+    function payTheBill(callback) {
+        return callback(true);
+    }
 ], function (err, result) {
+    console.log(err, result);
+
    // result now equals 'done'    
 });
